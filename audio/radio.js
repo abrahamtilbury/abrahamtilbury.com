a47e90636ea83bbed6d6ebddfdc89fdf29c0e80c
abrahamtilbury <abrahamtilbury@gmail.com>
2026-08-29 02:45:18 +0800

    Add track list to radio.js

---
+++ audio/radio.js
@@ -0,0 +1,47 @@
+const TRACKS = [
+    {
+        id: "butterflies",
+        title: "Butterflies",
+        file: "butterflies.mp3"
+    },
+    {
+        id: "citylights",
+        title: "City Lights",
+        file: "citylights.mp3"
+    },
+    {
+        id: "dreams",
+        title: "Dreams",
+        file: "dreams.mp3"
+    },
+    {
+        id: "opmyeyes",
+        title: "Op My Eyes",
+        file: "opmyeyes.mp3"
+    },
+    {
+        id: "photons",
+        title: "Photons",
+        file: "photons.mp3"
+    },
+    {
+        id: "pineapple",
+        title: "Pineapple",
+        file: "pineapple.mp3"
+    },
+    {
+        id: "racks",
+        title: "Racks",
+        file: "racks.mp3"
+    },
+    {
+        id: "song",
+        title: "Song",
+        file: "song.mp3"
+    },
+    {
+        id: "soright",
+        title: "So Right",
+        file: "soright.mp3"
+    }
+];
